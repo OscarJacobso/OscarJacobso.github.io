a38da6180c69584a3e2a1a1df19fa07ce1ae4a05
OscarJacobso <92553451+OscarJacobso@users.noreply.github.com>
2025-06-10 18:00:36 +0200

    Update N-Body.js

--- Scripts/N-Body.js
+++ Scripts/N-Body.js
@@ -85,10 +85,10 @@ class Body {
 // x, y, mass, vx, vy, size, colour
 const bodies = [
   new Body(canvas.width/2, canvas.height/2, 1650, 0, 0, 30, "yellow"),
-  new Body(canvas.width/2, canvas.height/2.5, 1, 500, 0, 8, "red"),
-  new Body(canvas.width/2, canvas.height/3, 1, 400, 0, 8, "blue"),
-  new Body(canvas.width/2, canvas.height/5, 1, 350, 0, 8, "orange"),
-  new Body(canvas.width/2, canvas.height/7, 1, 300, 0, 8, "green"),
+  new Body(canvas.width/2, canvas.height/2.5, 1, 500, 0, 8 + Math.random() * 4, "red"),
+  new Body(canvas.width/2, canvas.height/3, 1, 400, 0, 8 + Math.random() * 4, "blue"),
+  new Body(canvas.width/2, canvas.height/5, 1, 350, 0, 8 + Math.random() * 4, "orange"),
+  new Body(canvas.width/2, canvas.height/7, 1, 300, 0, 8 + Math.random() * 4, "green"),
 ];
 
 // create some random celestial bodies
